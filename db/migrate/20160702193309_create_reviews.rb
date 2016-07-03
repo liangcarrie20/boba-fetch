26e4324654c7b6bc65151fed33b21009dbb18b64
Carrie Liang <20.c.liang@gmail.com>
2016-07-02 20:05:01 -0700

    edit review model and migration

--- db/migrate/20160702193309_create_reviews.rb
+++ db/migrate/20160702193309_create_reviews.rb
@@ -1,4 +1,12 @@
 class CreateReviews < ActiveRecord::Migration
   def change
+    create_table :reviews do |t|
+      t.integer :rating, presence: true
+      t.string :body, presence: true
+      t.integer :user_id, presence: true
+      t.integer :drink_id, presence: true
+      t.integer :shop_id, presence: true
+
+      t.timestamps(null: false)
   end
 end
