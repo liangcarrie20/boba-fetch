3cc49a457a1b52b0eec69acacfc274da76b0b5a5
Carrie Liang <20.c.liang@gmail.com>
2016-07-02 20:24:22 -0700

    edit shop models and migrations

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,10 @@
 class Shop < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+
+  has_many :reviews
+  has_many :drinks
 end
--- db/migrate/20160702193318_create_shops.rb
+++ db/migrate/20160702193318_create_shops.rb
@@ -1,4 +1,13 @@
 class CreateShops < ActiveRecord::Migration
   def change
+    create_table :shops do |t|
+      t.string :name, presence: true
+      t.string :address, presence: true
+      t.string :city, presence: true
+      t.string :state, presence: true
+      t.integer :zip, presence: true
+
+      t.timestamps(null: false)
+    end
   end
 end
