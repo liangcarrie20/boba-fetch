c14c9e5e8b2158a3259e1dae04a978007b5b781a
Carrie Liang <20.c.liang@gmail.com>
2016-07-02 21:52:54 -0700

    add user controllers

---
+++ app/controllers/users.rb
@@ -0,0 +1,45 @@
+get '/users' do
+  redirect '/users/:id'
+end
+
+get '/users/new' do
+  erb :'users/new'
+end
+
+post "/users" do
+  @user = User.new(params[:user])
+
+  if @user.save
+    redirect "/sessions/new"
+  else
+    @error = @user.errors.full_messages
+    erb :"/users/new"
+  end
+end
+
+get "/users/:id" do
+  @user = current_user
+  erb :"/users/show"
+end
+
+get '/sessions/new' do
+  erb :'/sessions/new'
+end
+
+
+post "/sessions" do
+  @user = User.find_by(username: params[:user][:username])
+
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect "/users/:id"
+  else
+    @error = "Your email and or password was incorrect."
+    redirect '/sessions/new'
+  end
+end
+
+delete "/sessions/:id" do
+  session[:user_id] = nil
+  redirect "/"
+end
