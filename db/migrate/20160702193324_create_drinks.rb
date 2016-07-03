4378d5969aefcdc8b719bdd66bf7e4d3a8955107
Carrie Liang <20.c.liang@gmail.com>
2016-07-02 20:13:02 -0700

    edit drink model and migration

--- db/migrate/20160702193324_create_drinks.rb
+++ db/migrate/20160702193324_create_drinks.rb
@@ -1,4 +1,9 @@
 class CreateDrinks < ActiveRecord::Migration
   def change
+    create_table :drinks do |t|
+      t.string :name
+      t.integer :shop_id
+
+      t.timestamps(null: false)
   end
 end
