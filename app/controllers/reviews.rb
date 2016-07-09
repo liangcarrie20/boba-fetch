02a886066c7bf6f40318a2618e18e62812cdf153
Carrie Liang <20.c.liang@gmail.com>
2016-07-08 23:37:55 -0700

    update review controller for adding new review

--- app/controllers/reviews.rb
+++ app/controllers/reviews.rb
@@ -7,14 +7,17 @@ get '/shops/:shop_id/reviews' do
 end
 
 # Show form for creating a new review
-get '/shops/:id/reviews/new' do
-  @shop = Shop.find(params[:id])
+get '/shops/:shop_id/reviews/new' do
+  @reviews = Review.where(shop_id: params[:shop_id])
+  @drinks = Drink.where(shop_id: params[:shop_id])
+  @shop = Shop.find(params[:shop_id])
     erb :'reviews/new'
 end
 
 # Submit form and create a new review
-post '/shops/:id' do
-  @shop = Shop.find(params[:id])
+post '/shops/:shop_id/reviews' do
+  @shop = Shop.find(params[:shop_id])
+  @drinks = Drink.where(shop_id: params[:shop_id])
   @review = Review.new(params[:review])
 
   if @review.save
@@ -25,32 +28,32 @@ post '/shops/:id' do
   end
 end
 
-# Show form to edit review
-get '/shops/:id/reviews/:review_id/edit' do
-  @review = Review.find(params[:id])
-  erb :'reviews/edit'
+# # Show form to edit review
+# get '/shops/:id/reviews/:review_id/edit' do
+#   @review = Review.find(params[:id])
+#   erb :'reviews/edit'
 
-end
+# end
 
-# Submit form to edit review
-put '/shops/:id/reviews/:shop_id' do
-  @shop = Shop.find(params[:id])
-  @review = @shop.reviews.find(params[:shop_id])
+# # Submit form to edit review
+# put '/shops/:id/reviews/:shop_id' do
+#   @shop = Shop.find(params[:id])
+#   @review = @shop.reviews.find(params[:shop_id])
 
-  if @review.update_attributes(content: params[:content])
-    redirect "/shops/#{@shop.id}"
-  else
-    @errors = @review.errors.full_messages
-    erb :'reviews/edit'
-  end
-end
+#   if @review.update_attributes(content: params[:content])
+#     redirect "/shops/#{@shop.id}"
+#   else
+#     @errors = @review.errors.full_messages
+#     erb :'reviews/edit'
+#   end
+# end
 
-# Delete review
-delete '/shop/:id/reviews/:shop_id' do
-  @shop = Shop.find(params[:id])
-  @review = @shop.reviews.find(params[:shop_id])
+# # Delete review
+# delete '/shop/:id/reviews/:shop_id' do
+#   @shop = Shop.find(params[:id])
+#   @review = @shop.reviews.find(params[:shop_id])
 
-  @review.destroy
+#   @review.destroy
 
-  redirect "/shop/#{@shop.id}"
-end
+#   redirect "/shop/#{@shop.id}"
+# end
