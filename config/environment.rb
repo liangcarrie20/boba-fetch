f5e117db53da3fb5ddc194a75c5446c5a9bf2377
Carrie Liang <20.c.liang@gmail.com>
2016-07-09 14:09:01 -0700

    update enviornment and gems

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 require 'dotenv'
-# Dotenv.load
-# SECRET_TOKEN = ENV['SECRET_TOKEN']
+Dotenv.load
+SECRET_TOKEN = ENV['SECRET_TOKEN']
 
 # Require gems we care about
 require 'rubygems'
