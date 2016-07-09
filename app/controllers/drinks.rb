1dc3f5d6de70c1932981326d5ec8c5330da8546e
Carrie Liang <20.c.liang@gmail.com>
2016-07-08 23:36:59 -0700

    update drink controller for adding drinks

--- app/controllers/drinks.rb
+++ app/controllers/drinks.rb
@@ -2,8 +2,8 @@
 get '/shops/:shop_id/drinks' do 
 
   @shop = Shop.find(params[:shop_id])
-
-  @drinks = @shop.drinks
+  @reviews = Review.where(shop_id: params[:shop_id])
+  @drinks = Drink.where(shop_id: params[:shop_id])
 
   erb :'drinks/index'
 
@@ -11,7 +11,7 @@ end
 
 # returns a form for creating a new drink belonging to a specific shop
 get '/shops/:shop_id/drinks/new' do 
-
+  @reviews = Review.where(shop_id: params[:shop_id])
   @shop = Shop.find(params[:shop_id])
 
   erb :'drinks/new'
@@ -26,7 +26,7 @@ post '/shops/:shop_id/drinks' do
   @drink = @shop.drinks.new(params[:drink])
 
   if @drink.save
-    redirect "/shops/#{@shop.id}/drinks"
+    redirect "/shops/#{@shop.id}/drinks/#{@drink.id}"
   else
     @errors = @review.errors.full_messages
     erb :'drinks/new'
@@ -36,50 +36,49 @@ end
 
 # display a specific drink belonging to a specific shop
 get '/shops/:shop_id/drinks/:id' do 
-
   @shop = Shop.find(params[:shop_id])
-
-  @drink = @shop.drinks.find(params[:id])
+  @reviews = Review.where(drink_id: params[:id])
+  @drink = Drink.find(params[:id])
 
   erb :'drinks/show'
 
 end
 
-# returns a form for editing drink belonging to a specific shop
-get '/shops/:shop_id/drinks/:id/edit' do
+# # returns a form for editing drink belonging to a specific shop
+# get '/shops/:shop_id/drinks/:id/edit' do
 
-  @shop = Shop.find(params[:shop_id])
+#   @shop = Shop.find(params[:shop_id])
 
-  @drink = @shop.drinks.find(params[:id])
+#   @drink = @shop.drinks.find(params[:id])
 
-  erb :'drinks/edit'
+#   erb :'drinks/edit'
 
-end
+# end
 
-# update a specific drink belonging to a specific shop
-put '/shops/:shop_id/drinks/:id' do
+# # update a specific drink belonging to a specific shop
+# put '/shops/:shop_id/drinks/:id' do
 
-  @shop = Shop.find(params[:shop_id])
+#   @shop = Shop.find(params[:shop_id])
 
-  @drink = @shop.drinks.find(params[:id])
+#   @drink = @shop.drinks.find(params[:id])
 
-  if @drink.update_attributes(params[:drink])
-    redirect "/shops/#{@shop.id}/drinks"
-  else 
-    erb :'drinks/edit' #show edit drinks view again(potentially displaying errors)
-  end
+#   if @drink.update_attributes(params[:drink])
+#     redirect "/shops/#{@shop.id}/drinks"
+#   else 
+#     erb :'drinks/edit' #show edit drinks view again(potentially displaying errors)
+#   end
 
-end
+# end
 
-# delete a drink belonging to a specific shop
-delete '/shops/:shop_id/drinks/:id' do 
+# # delete a drink belonging to a specific shop
+# delete '/shops/:shop_id/drinks/:id' do 
 
-  @shop = Shop.find(params[:shop_id])
+#   @shop = Shop.find(params[:shop_id])
 
-  @drink = @shop.drinks.find(params[:id])
+#   @drink = @shop.drinks.find(params[:id])
 
-  @drink.destroy
+#   @drink.destroy
 
-  redirect "/shops/#{@shop.id}/drinks"
+#   redirect "/shops/#{@shop.id}/drinks"
 
-end
+# end
