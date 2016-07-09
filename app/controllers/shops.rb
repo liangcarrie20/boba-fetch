e5378d1d8f1538f02db2ea1d92db79f298bbe023
Carrie Liang <20.c.liang@gmail.com>
2016-07-08 20:28:18 -0700

    add shops controller

---
+++ app/controllers/shops.rb
@@ -0,0 +1,43 @@
+get '/shops' do
+  @shops = Shop.all
+  erb :'/shops/index'
+end
+
+get '/shops/new' do
+  erb :'/shops/new'
+end
+
+post '/shops' do
+  @shop = Shop.new(params[:shop])
+  if @shop.save
+    redirect '/shops'
+  else
+    @errors = @shop.errors.full_messages
+    redirect '/shops/new'
+  end
+end
+
+get '/shops/:id' do
+  @shop = Shop.find(params[:id])
+  @reviews = Review.where(shop_id: params[:id])
+  erb :'/shops/show'
+end
+
+get '/shops/:id/edit' do
+  @shop = Shop.find(params[:id])
+  erb :'/shops/edit'
+end
+
+put '/shops/:id' do
+  @shop = Shop.find(params[:id])
+  @shop.update_attributes(params[:shop])
+  redirect "/shops/"
+end
+
+delete '/shops/:id' do
+  @shop = Shop.find(params[:id])
+
+  @shop.destroy
+
+  redirect '/shops'
+end
