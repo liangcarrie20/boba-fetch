9bbea7be9c8d369260380f4e0376481f1b23b31a
Carrie Liang <20.c.liang@gmail.com>
2016-07-09 16:18:44 -0700

    add map to shop show page

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,11 @@
+var latLng = {lat: 37.774929, lng: -122.419416}
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: latLng,
+    zoom: 11
+  });
+}
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
