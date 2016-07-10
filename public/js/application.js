dd438809fa74d7de66bd7495a51284b08775e22e
Carrie Liang <20.c.liang@gmail.com>
2016-07-09 20:51:05 -0700

    add marker to map

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,31 @@
+var geocoder;
+var map;
+
 function initMap() {
+  geocoder = new google.maps.Geocoder();
   var latlng = new google.maps.LatLng(37.774929, -122.419416);
   var mapOptions = {
-    zoom: 11,
+    zoom: 15,
     center: latlng
   }
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  document.getElementById('submit').addEventListener('click', function() {
+  geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
