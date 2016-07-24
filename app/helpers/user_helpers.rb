0562b1c023904f6de87cc2f1881e3f7d365af768
Carrie Liang <20.c.liang@gmail.com>
2016-07-23 20:20:46 -0700

    refactor current_user method. add login and logout helper methods

--- app/helpers/user_helpers.rb
+++ app/helpers/user_helpers.rb
@@ -1,5 +1,13 @@
 helpers do
+  def login(user)
+    session[:id] = user.id
+  end
+
   def current_user
-    @current_user ||=User.find_by(id: session[:user_id])
+    @current_user ||=User.find(session[:id]) if session[:id]
+  end
+
+  def logout
+    session[:id] = nil
   end
 end
