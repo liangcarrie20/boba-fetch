74a0432f74d693f4726996b232734da55d3f5f3c
Carrie Liang <20.c.liang@gmail.com>
2016-07-23 20:06:49 -0700

    remove unecessary authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,4 @@ class User < ActiveRecord::Base
     self.username = self.username.downcase
     self.email = self.email.downcase
   end
-
-  def self.authenticate(username, password)
-    @user = User.find_by(username: username)
-    if @user.password == password
-      return @user
-    else
-      nil
-    end
-  end
 end
