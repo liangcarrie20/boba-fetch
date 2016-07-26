a8d7b07d7aed1b5d3ea1ecaea0d50b5c5d699da9
Carrie Liang <20.c.liang@gmail.com>
2016-07-26 15:14:48 -0700

    require sinatra/flash in environment

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ require 'bcrypt'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 require 'faker'
+require 'sinatra-flash'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
