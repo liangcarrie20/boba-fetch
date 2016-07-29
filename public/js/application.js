a027f7af6cb23ea384655d8c8eb1d1c67f035abf
Carrie Liang <20.c.liang@gmail.com>
2016-07-28 18:23:53 -0700

    refactor marker function and call marker function inside of initMap

--- public/js/application.js
+++ public/js/application.js
@@ -1,31 +1,28 @@
-var geocoder;
-var map;
-
-function initMap() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(37.774929, -122.419416);
-  var mapOptions = {
-    zoom: 15,
-    center: latlng
+  var geocoder;
+  var map;
+  function initMap() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(37.774929, -122.419416);
+    var mapOptions = {
+      zoom: 14,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    codeAddress();
   }
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  document.getElementById('submit').addEventListener('click', function() {
-  geocodeAddress(geocoder, map);
-  });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
+function codeAddress() {
   var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
+  geocoder.geocode( {'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
+          map: map,
+          position: results[0].geometry.location
       });
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
 }
+
